b0ba45aec4ed6a8d1ed1b98569462c5f0c337f2e
nicholasscabral <nicholasscabral@gmail.com>
2020-07-06 20:52:49 -0300

    correcting errors

--- calc.js
+++ calc.js
@@ -1,9 +1,21 @@
 function insert(num) {
+    //if there is syntax error, return the function
+    if(SyntaxError) { 
+        return
+    }
+    
     // insert a number into the display
     if(display.value.length < 20) {
-
-        display.value +=  num
-    
+      
+        if(isNaN(num)) {
+            display.value += num
+        }
+        else if(display.value.length == 1 && display.value[0] == 0) {
+            display.value = num
+        }
+        else {
+            display.value += num
+        }
     }
     else {
         return
@@ -11,8 +23,11 @@ function insert(num) {
 }
 
 function clean() {
+    //if there is a syntax error, toggle the variable to false (reset)
+    SyntaxError = false
     //clear the display value 
-    display.value = "";
+    display.value = "0";
+
 }
 
 function equal() {
@@ -23,6 +38,7 @@ function equal() {
     // if there is a syntax error, show in the display
     if(error) {
         display.value = "Syntax Error"
+        SyntaxError = true
     }
 
     for(i = 0; i < exp.length; i++) {
@@ -31,6 +47,7 @@ function equal() {
             if(exp[i] == "*" || exp[i] == "/") {
                 flag = true; //if there is at least one math operation before any number, flag = true
                 display.value = "Syntax Error" 
+                SyntaxError = true
             }
         }
         break
@@ -38,11 +55,13 @@ function equal() {
 
     if(flag == false) { //if there is no  errors, calculate the expression normaly
         var answer = eval(exp)
+
         if(isFinite(answer)) {
             display.value = answer
         }
         else {
             display.value = "Math Error" // if is infinity 
+            SyntaxError = true
         }
     }
    
@@ -50,28 +69,47 @@ function equal() {
 }
 
 function back() {
-    
+    //if there is syntax error, return the function
+    if(SyntaxError) {
+        return
+    }
+
     display.value = display.value.substring(0,display.value.length-1)
+    
+    if(display.value == "") {
+        display.value = "0"
+    }
 
 }
-//selecting all the buttons
-const buttons = document.querySelectorAll('.button')
+
 //selecting display
 const display = document.querySelector('.display')
-//adding event listener for each button in "buttons"
-buttons.forEach( (button) => {
+//selecting all numbers
+const numbers = document.querySelectorAll('.number')
+//adding event listener for each number in "numbers"
+numbers.forEach( (button) => {
     button.addEventListener('click', calculate)
 })
+//selecting all operators
+const operators = document.querySelectorAll('.operator')
+//adding event listener for each operator in "operators"
+operators.forEach( (button) => {
+    button.addEventListener('click', calculate)
+})
+
+//boolean variable to check if there is syntax error
+var SyntaxError = false
 
 function calculate(event) {
     var buttonValue = event.target.value
 
-    if (!isNaN(buttonValue) || buttonValue == "+" || buttonValue == "-" || buttonValue == "x" || buttonValue == "/" || buttonValue == ".") {
+
+    if (!isNaN(buttonValue) || (isNaN(buttonValue) && buttonValue != "=" && buttonValue != "<" && buttonValue != "c")) {
         if(buttonValue == "x") {
             buttonValue = "*" //changing the "x" into "*" to calculate normally
         }
 
-        //if the value is a number, insert that number 
+        //insert the buttonValue 
         insert(buttonValue) 
 
     }
@@ -86,6 +124,5 @@ function calculate(event) {
     }
     
 }
-
 
 
